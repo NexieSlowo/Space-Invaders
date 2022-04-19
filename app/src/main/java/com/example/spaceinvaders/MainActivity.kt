ed747d7dae5f4ac9e3deb2e1f8510f83f658933d
NexieSlowo <62484829+NexieSlowo@users.noreply.github.com>
2022-04-19 23:12:55 +0200

    musique implémentée

--- app/src/main/java/com/example/spaceinvaders/MainActivity.kt
+++ app/src/main/java/com/example/spaceinvaders/MainActivity.kt
@@ -1,28 +1,61 @@
 package com.example.spaceinvaders
 
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
+import android.net.Uri
+
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
 class MainActivity : AppCompatActivity() {
-
-
-
-
+    private var mediaPlayer : MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val musicSWITCH = findViewById<Switch>(R.id.switch2)
         val changeActivityBTN = findViewById<Button>(R.id.button1)
+
         changeActivityBTN.setOnClickListener{
+            stopAudio()
             val intent = Intent(this,SecondActivity::class.java)
             startActivity(intent)
         }
+        musicSWITCH.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked) {
+                playAudio()
+            } else {
+                pauseAudio()
+            }
+        }
 
     }
+    private fun playAudio() {
+        if(mediaPlayer == null){
+            mediaPlayer = MediaPlayer.create(this, R.raw.cantina_band)
+        }
+        mediaPlayer?.start()
+        Toast.makeText(this,"Music started", Toast.LENGTH_SHORT).show()
+    }
+    private fun pauseAudio() {
+        if (mediaPlayer?.isPlaying() == true){
+            mediaPlayer?.pause()
 
+            Toast.makeText(this,"Music paused", Toast.LENGTH_SHORT).show()
+        }
 
-
+    }
+    private fun stopAudio(){
+        if (mediaPlayer?.isPlaying() == true){
+            mediaPlayer?.stop()
+            mediaPlayer = null
+            Toast.makeText(this,"Music stopped", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
--- app/src/main/java/com/example/spaceinvaders/SecondActivity.kt
+++ app/src/main/java/com/example/spaceinvaders/SecondActivity.kt
@@ -1,13 +1,16 @@
 package com.example.spaceinvaders
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SecondActivity : AppCompatActivity() {
-
+    private var mediaPlayer : MediaPlayer? = null
     lateinit var spaceview: SpaceView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +18,26 @@ class SecondActivity : AppCompatActivity() {
         setContentView(R.layout.activity_second)
         spaceview = findViewById(R.id.vMain)
 
+        val musicSWITCH = findViewById<Switch>(R.id.switch1)
         val changeActivityBTN = findViewById<FloatingActionButton>(R.id.floatingActionButton2)
+        val pauseBTN = findViewById<FloatingActionButton>(R.id.floatingActionButton1)
+
         changeActivityBTN.setOnClickListener{
+            stopAudio()
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
+        }
+        pauseBTN.setOnClickListener {
+            pauseAudio()
+            musicSWITCH.isChecked=false
+        }
+
+        musicSWITCH.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked) {
+                playAudio()
+            } else {
+                pauseAudio()
+            }
         }
     }
     override fun onPause() {
@@ -29,5 +48,27 @@ class SecondActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         spaceview.resume()
+    }
+    private fun playAudio() {
+        if(mediaPlayer == null){
+            mediaPlayer = MediaPlayer.create(this, R.raw.battle_of_the_heroes)
+        }
+        mediaPlayer?.start()
+        Toast.makeText(this,"Music started", Toast.LENGTH_SHORT).show()
+    }
+    private fun pauseAudio() {
+        if (mediaPlayer?.isPlaying() == true){
+            mediaPlayer?.pause()
+
+            Toast.makeText(this,"Music paused", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    private fun stopAudio(){
+        if (mediaPlayer?.isPlaying() == true){
+            mediaPlayer?.stop()
+            mediaPlayer = null
+            Toast.makeText(this,"Music stopped", Toast.LENGTH_SHORT).show()
+        }
     }
 }
